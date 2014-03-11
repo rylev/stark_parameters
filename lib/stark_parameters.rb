3af7d3a218727a27cdcf30079261cefe81b0bdd1
Ryan Levick <ryan@6wunderkinder.com>
2014-03-11 17:59:03 +0100

    Start move to new look

--- lib/stark_parameters.rb
+++ lib/stark_parameters.rb
@@ -1,56 +1,76 @@
 module StarkParameters
-  class Validator
 
-    def initialize(params)
-      @params = make_strong_parameter(params)
-      @permitted_params = make_strong_parameter({})
-    end
+  def self.included(klass)
+    klass.send :extend, ClassMethods
+    klass.required_params = []
+    klass.permitted_params = []
+    klass.aliases = {}
+  end
 
-    def params
-      make_strong_parameter(@permitted_params.empty? ? @params : @permitted_params)
-    end
+  def initialize(params)
+    @params = make_strong_parameter(params)
+  end
 
-    def require(*to_require)
-      to_require.each { |required_param| @params.require(required_param) }
-      self
-    end
+  def params
+    @params = permitted_params.merge(required_params)
+  end
 
-    def require_as(aliases)
-      params = @params.dup
-      aliases.each do |old_key, new_key|
-        params.require(old_key)
-        params[new_key] = params.delete(old_key)
+  private
+
+  def permitted_params
+    self.class.permitted_params.each_with_object({}) do |permitted_param, hash|
+      param_key = if permitted_param.is_a? Hash
+       permitted_param.keys.first
+      else
+        permitted_param
       end
-      @params = make_strong_parameter(params)
-      self
+      hash[(self.class.aliases[param_key] || param_key).to_s] = @params.permit(permitted_param).values.first
     end
+  end
 
-    def require_one(*to_require)
-      unless to_require.any? {|p| @params.include?(p) }
-        raise ActionController::ParameterMissing.new(to_require.join(" or "))
+  def required_params
+    self.class.required_params.each_with_object({}) do |required_params, hash|
+      param_key = require_one(required_params)
+      hash[(self.class.aliases[param_key] || param_key).to_s] = @params[param_key]
+    end
+  end
+
+
+  def make_strong_parameter(hash)
+    return hash if hash.is_a? ActionController::Parameters
+    ActionController::Parameters.new(hash)
+  end
+
+  def require_one(to_require)
+    present_param = to_require.detect {|p| @params.include?(p) }
+    unless present_param
+      raise ActionController::ParameterMissing.new(to_require.join(" or "))
+    end
+    present_param
+  end
+
+  module ClassMethods
+    attr_accessor :required_params, :permitted_params, :aliases
+
+    def require(required_params, options = {})
+      required_params = Array(required_params)
+      if new_name = options[:as]
+        required_params.each { |rp| @aliases[rp] = new_name }
       end
-      self
+      @required_params.push required_params
     end
 
-    def permit(*to_permit)
-      @permitted_params = @permitted_params.merge(@params.permit(*to_permit))
-      self
-    end
-
-    def permit_as(aliases)
-      @permitted_params = aliases.each_with_object({}) do |(old_key, new_key), hash|
-        if value = @params[old_key]
-          hash[new_key] = value
+    def permit(permitted_param, options = {})
+      if new_name = options[:as]
+        param_key = if permitted_param.is_a? Hash
+          permitted_param.keys.first
+        else
+          permitted_param
         end
-      end.merge(@permitted_params)
-      self
-    end
 
-    private
-
-    def make_strong_parameter(hash)
-      return hash if hash.is_a? ActionController::Parameters
-      ActionController::Parameters.new(hash)
+        @aliases[param_key] = new_name
+      end
+      @permitted_params.push permitted_param
     end
   end
 end
--- spec/lib/validator_spec.rb
+++ spec/lib/validator_spec.rb
@@ -1,22 +1,46 @@
 require "spec_helper"
 
-describe StarkParameters::Validator do
+describe StarkParameters do
+  let(:test_klass) do
+    Class.new do
+      include StarkParameters
+
+      require :name
+      require :id
+      require :email, as: :login
+      permit  author: [:id, :name]
+      permit  :password, as: :pword
+      require [:last_name, :surname]
+    end
+  end
+
   let(:raw_params) do
     {
       "id" => 1,
       "name" => "Ryan",
       "email" => "ryan@6wunderkinder.com",
       "password" => "fdsafdsa",
+      "surname" => "Levick",
       "author" => { "id" => 1, "name" => "Steve" }
     }
   end
 
-  let(:params) { ActionController::Parameters.new(raw_params) }
-  let(:validator) { described_class.new(params) }
+  let(:validator) { test_klass.new(raw_params) }
 
   describe "#require" do
     context "when param is there" do
-      it { expect(validator.require(:name).params.to_hash).to eql(raw_params) }
+      let(:new_params) do
+        {
+          "id" => 1,
+          "name" => "Ryan",
+          "login" => "ryan@6wunderkinder.com",
+          "pword" => "fdsafdsa",
+          "surname" => "Levick",
+          "author" => { "id" => 1, "name" => "Steve" }
+        }
+      end
+
+      it { expect(validator.params.to_hash).to eql(new_params) }
     end
 
     context "when param is missing" do
